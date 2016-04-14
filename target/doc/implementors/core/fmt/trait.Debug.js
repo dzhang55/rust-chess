727586eda427aa991f92f3a8dd1b54098cda301f
Daniel Zhang <dzhang55@gmail.com>
2016-04-13 23:08:54 -0400

    Initial target/doc subtree commit

---
+++ target/doc/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['chess'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='chess/chess_server/enum.Action.html' title='chess::chess_server::Action'>Action</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='chess/board/struct.Cell.html' title='chess::board::Cell'>Cell</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ target/doc/implementors/rustc_serialize/serialize/trait.Encodable.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['chess'] = ["impl <a class='trait' href='https://doc.rust-lang.org/rustc-serialize/rustc_serialize/serialize/trait.Encodable.html' title='rustc_serialize::serialize::Encodable'>Encodable</a> for <a class='enum' href='chess/chess_server/enum.Action.html' title='chess::chess_server::Action'>Action</a>","impl <a class='trait' href='https://doc.rust-lang.org/rustc-serialize/rustc_serialize/serialize/trait.Encodable.html' title='rustc_serialize::serialize::Encodable'>Encodable</a> for <a class='struct' href='chess/chess_server/struct.Payload.html' title='chess::chess_server::Payload'>Payload</a>","impl <a class='trait' href='https://doc.rust-lang.org/rustc-serialize/rustc_serialize/serialize/trait.Encodable.html' title='rustc_serialize::serialize::Encodable'>Encodable</a> for <a class='struct' href='chess/board/struct.Cell.html' title='chess::board::Cell'>Cell</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
