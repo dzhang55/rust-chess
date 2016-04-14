439c6915eb8691c419739386a679721f995b9763
Daniel Zhang <dzhang55@gmail.com>
2016-04-13 22:48:04 -0400

    Initial doc subtree commit

---
+++ target/doc/chess/board/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Piece","Represents the possible types of pieces in a given cell."]],"struct":[["Board","Represents all the pieces in the board."],["Cell","Represents the index of a single cell in the board."]]});
